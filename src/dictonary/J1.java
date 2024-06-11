301697b67992d6d8c296bda22ae715ce3a06bddc
setthi-pheap <157299176+setthi-pheap@users.noreply.github.com>
2024-06-11 18:33:43 +0700

    New

--- src/dictonary/J1.java
+++ src/dictonary/J1.java
@@ -1,8 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+
  */
+
 package dictonary;
+
 
 /**
  *
@@ -293,6 +296,7 @@ public class J1 extends javax.swing.JFrame {
             }
         });
     }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
