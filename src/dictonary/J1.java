edc26e40d4a0c2dcfcf6016c2b3761528fb13d28
Reth-Sela <selareth7@gmail.com>
2024-06-13 16:24:22 +0700

    jom plex push

--- src/dictonary/J1.java
+++ src/dictonary/J1.java
@@ -234,7 +234,9 @@ public class J1 extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+            historyForm hisform =new historyForm();
+            hisform.setVisible(true);
+            this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
